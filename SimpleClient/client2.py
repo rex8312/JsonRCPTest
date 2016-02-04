49c6ed57d74f4f6d7556cc38282b6492bcfeae85
rex8312 <rex8312@gmail.com>
2016-02-04 10:08:32 +0900

    ..

--- SimpleClient/client2.py
+++ SimpleClient/client2.py
@@ -15,7 +15,7 @@ class Worker(object):
         self.headers = {'content-type': 'application/json'}
         self.id = 0
         self.post = functools.partial(requests.post, self.url, headers=self.headers)
-        self.pool = ThreadPool(1000)
+        self.pool = ThreadPool(100)
 
     def _call(self, method, *args):
         payload = dict(method=method, params=args, jsonrpc="2.0", id=self.id)
@@ -45,22 +45,21 @@ class Worker(object):
 class Future:
     def __init__(self, result):
         self.result = result
+        self.true_result = None
 
     def __repr__(self):
         return str(self.get())
 
     def get(self):
-        # return self.result.get()
-        return self.result.get().get('result', self.result.get().get('error'))
+        return self.true_result or self.result.get().get('result', self.result.get().get('error'))
 
     def complete(self):
         return self.result.ready()
 
     def on_complete(self, func):
         def _func(arg):
-            print(arg)
-            print(arg.get())
-            func(arg.get().get('result', arg.get().get('error')))
+            self.true_result = arg.get().get('result', arg.get().get('error'))
+            func(self.true_result)
 
         self.result.rawlink(_func)
         return self
@@ -101,25 +100,38 @@ class FutureManager:
 if __name__ == "__main__":
     worker = Worker('http://127.0.0.1:8080/rpc')
 
-    def func1():
-        print("blocking")
+    def blocking():
+        print("** blocking **")
         stime = time.clock()
         print(worker.ask("power", 2, 6).get())
         print(worker.ask("echo", "echome!").get())
         print(time.clock() - stime)
         print()
 
-    def func2():
-        print("non-blocking")
+    def non_blocking():
+        print("** non-blocking **")
         stime = time.clock()
-        for idx in range(1000):
-            #worker.ask("power", random.randint(0, 10), random.randint(0, 10)).on_complete(print)
-            worker.ask("echo", idx).on_complete(print)
-        # r2 = worker.ask("echo", random.randint(0, 10)).on_complete(print)
-        # r3 = worker.ask("echo", random.randint(0, 10)).on_complete(print)
-        print(time.clock() - stime)
-        worker.join()
+        worker.tell("power", random.randint(0, 10), random.randint(0, 10))
+        worker.tell("echo", "echome!")
+        print("elapsed time: (tell message): {}".format(time.clock() - stime))
+        
+        stime = time.clock()
+        r1 = worker.ask("power", random.randint(0, 10), random.randint(0, 10)).on_complete(print)
+        r2 = worker.ask("echo", "echome!").on_complete(print)
+        ttime = time.clock() - stime
+        print("elapsed time (send messages): {}".format(time.clock() - stime))
+        
+        print("do other jobs")
+        time.sleep(15)        
+        
+        stime = time.clock()
+        print("get", r1.get())
+        print("get", r2.get())
+        ttime += time.clock() - stime
+        print("elapsed time (get respond): {}".format(time.clock() - stime))
 
-    #func1()
-    func2()
+        print("total elapsed time: {}".format(ttime))
+
+    blocking()
+    non_blocking()
 
